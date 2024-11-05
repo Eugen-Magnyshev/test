fe2e08bb954b9caaf9569449af9830789be245a7
Евгений Магнышев <magnysevevgenij@gmail.com>
2024-11-05 21:22:24 +0000

    nm

---
+++ hello.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var lang:= ReadString('ведите язык (ru, en, de): ');
+  
+  case lang of
+    'ru': Writeln('Привет, мир!');
+    'en': Writeln('Hello, world!');
+    'de': Writeln('Hallo Welt!');
+  else Writeln('Неизвестный язык');
+  end;
+end.
